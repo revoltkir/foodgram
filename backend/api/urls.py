97bc88679301afaa26fb7af6e58a693a10ed670f
Kirill Tikach <coolk@mail.ru>
2025-06-20 20:48:04 +0300

    add urls

---
+++ backend/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import RecipeViewSet
+
+router = DefaultRouter()
+router.register(r'recipes', RecipeViewSet, basename='recipes')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
