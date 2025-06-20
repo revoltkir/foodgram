aa275aff4ef9cae60f20340422e20288f9396fb4
Kirill Tikach <coolk@mail.ru>
2025-06-20 20:47:38 +0300

    add RecipeReadSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,14 +1,48 @@
 from rest_framework import serializers
-from recipes.models import Tag, Ingredient
+from recipes.models import Tag, Ingredient, RecipeIngredient, Recipe
 
 
 class TagSerializer(serializers.ModelSerializer):
+    """ Сериализатор для модели Tag."""
+
     class Meta:
         model = Tag
         fields = ('id', 'name', 'color', 'slug')
 
 
 class IngredientSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Ingredient."""
+
     class Meta:
         model = Ingredient
         fields = ('id', 'name', 'measurement_unit')
+
+
+class RecipeIngredientSerializer(serializers.ModelSerializer):
+    """Сериализатор для промежуточной модели RecipeIngredient."""
+    name = serializers.ReadOnlyField(source='ingredient.name')
+    measurement_unit = serializers.ReadOnlyField(
+        source='ingredient.measurement_unit')
+
+    class Meta:
+        model = RecipeIngredient
+        fields = ('id', 'ingredient', 'name', 'measurement_unit', 'amount')
+
+
+class RecipeReadSerializer(serializers.ModelSerializer):
+    """Сериализатор для чтения рецепта."""
+    author = serializers.StringRelatedField(read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
+    ingredients = RecipeIngredientSerializer(
+        source='recipe_ingredients',
+        many=True,
+        read_only=True
+    )
+    image = serializers.ImageField(read_only=True)
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'id', 'name', 'author', 'ingredients', 'tags',
+            'image', 'text', 'cooking_time'
+        )
