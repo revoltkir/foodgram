90938058e040043a2f6b577737cc3679bc4dcec9
Kirill Tikach <coolk@mail.ru>
2025-06-20 20:18:02 +0300

    add IngredientSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from recipes.models import Tag
+from recipes.models import Tag, Ingredient
 
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ('id', 'name', 'color', 'slug')
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'name', 'measurement_unit')
