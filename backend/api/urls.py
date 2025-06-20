6008f197b4ef42a1db01ac2775a8963b5cad82c8
Kirill Tikach <coolk@mail.ru>
2025-06-20 21:02:31 +0300

    add debugtoolbar to settings

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from .views import RecipeViewSet
+from django.conf import settings
 
 router = DefaultRouter()
 router.register(r'recipes', RecipeViewSet, basename='recipes')
@@ -8,3 +9,7 @@ router.register(r'recipes', RecipeViewSet, basename='recipes')
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
