16e35c30a9dba2337371daef30472ecea3f062f8
Kirill Tikach <coolk@mail.ru>
2025-06-30 15:27:42 +0300

    add users admin.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import FoodgramUser, Subscription
+
+
+@admin.register(FoodgramUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'first_name', 'last_name')
+    list_filter = ('email', 'first_name')
+
+
+admin.site.register(Subscription)
