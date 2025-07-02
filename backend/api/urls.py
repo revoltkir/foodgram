2b3f36936e4f6ad6a34d1dab0501fba280230a52
Kirill Tikach <coolk@mail.ru>
2025-07-03 00:27:57 +0300

    add urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import RecipeViewSet,IngredientViewSet
+from .views import RecipeViewSet, IngredientViewSet, TagViewSet
 from django.conf import settings
 
 router = DefaultRouter()
+router.register('tags', TagViewSet, basename='tags')
+router.register('ingredients', IngredientViewSet, basename='ingredients')
 router.register(r'recipes', RecipeViewSet, basename='recipes')
-router.register(r'ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
     path('', include(router.urls)),
