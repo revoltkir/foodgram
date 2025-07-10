dd2153f5e4b92362d22deccecf2bab6b831cd071
Kirill Tikach <coolk@mail.ru>
2025-07-10 12:59:01 +0300

    some fix acoording review

--- backend/api/utils/item_action_mixin.py
+++ backend/api/utils/item_action_mixin.py
@@ -4,13 +4,14 @@ from rest_framework.response import Response
 
 
 class ItemActionMixin:
-    """
-    Миксин для добавления и удаления рецептов в избранное и корзину.
-    """
+    """Миксин для добавления и удаления рецептов в избранное и корзину."""
+
+    def get_recipe(self, model, pk):
+        recipe_model = model._meta.get_field('recipe').related_model
+        return get_object_or_404(recipe_model, pk=pk)
 
     def add_item(self, model, serializer_class, request, pk=None):
-        recipe = get_object_or_404(
-            model._meta.get_field('recipe').related_model, pk=pk)
+        recipe = self.get_recipe(model, pk)
         user = request.user
         if model.objects.filter(user=user, recipe=recipe).exists():
             return Response({'message': 'Рецепт уже добавлен'},
@@ -20,8 +21,7 @@ class ItemActionMixin:
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def remove_item(self, model, request, pk=None):
-        recipe = get_object_or_404(
-            model._meta.get_field('recipe').related_model, pk=pk)
+        recipe = self.get_recipe(model, pk)
         item = model.objects.filter(user=request.user, recipe=recipe)
         if not item.exists():
             return Response({'message': 'Рецепт не найден в списке.'},
